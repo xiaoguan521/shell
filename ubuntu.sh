87880ba4432b5618ef60bd7275d13c9f191678fc
xiaoguan521 <xiaochen1649@gmail.com>
2023-03-14 05:22:39 +0000

    一键脚本

---
+++ ubuntu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# 创建 ubuntu 目录并进入
+mkdir -p ubuntu && cd ubuntu
+
+# 创建 docker-compose.yml 文件并写入内容
+cat <<EOT >> docker-compose.yml
+version: '3.5'
+
+services:
+    ubuntu-xfce-vnc:
+        container_name: xfce
+        image: imlala/ubuntu-xfce-vnc-novnc:latest
+        shm_size: "2gb"
+        ports:
+            - 5555:5900
+            - 6666:6080
+        devices:
+            - /dev/snd
+        environment:
+            - VNC_PASSWD=xiaochen
+            - GEOMETRY=1920x1200
+            - DEPTH=24
+            - 'ALSADEV=hw:2,0'
+        volumes:
+            - ./Downloads:/root
+        restart: unless-stopped
+EOT
+
+# 启动容器
+docker-compose up -d
